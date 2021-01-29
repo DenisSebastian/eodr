1109a8564315994a462cc94e5bb93447d3d9460c
Denis Berroeta <denisberroeta@gmail.com>
2021-01-29 00:26:45 -0300

    modificacion de funciones ver tabla

--- R/conexiones.R
+++ R/conexiones.R
@@ -9,4 +9,3 @@ con_eod2017 <- function(){
   RSQLite::dbConnect(RSQLite::SQLite(),
                      paste0(system.file("extdata", package = "eodr"), "/eod2017.sqlite"))
 }
-
--- R/ver_tablas.R
+++ R/ver_tablas.R
@@ -1,12 +1,12 @@
-#' export
+#' @export
 ver_tablas_eod2017 <-  function(){
   con <-  con_eod2017()
   tibble::tibble(RSQLite::dbListTables(con))
 }
 
-#' export
+#' @export
 tabla <-  function(nombre){
   con <-  con_eod2017()
-  d <- dplyr::tbl(con, nombre) %>% collect()
+  d <- dplyr::tbl(con, nombre) %>% dplyr::collect()
   return(d)
 }
